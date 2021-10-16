b9cb5eb4406416087eb5f2a90d68bd86c0a0f9b5
Daedma <hdamir163@gmail.com>
2021-10-16 16:32:50 +0400

    Небольшая полировка кода + комментарии

--- sem1/inf/2_var27/main.cpp
+++ sem1/inf/2_var27/main.cpp
@@ -1,20 +1,22 @@
 #include <iostream>
 #include <cmath>
-#include <functional>
 #include <limits>
 #include <string>
 #include <array>
 #include <algorithm>
 #include <iomanip>
 
+//функция для подсчета высоты, с которой падало тело, по расстоянию, 
+//пройденным телом за последнюю секунду падения
 long double calc_height(const long double PathPart) noexcept
 {
-    static constexpr long double half_g = 4.9L;
-    return half_g * ((2.L - PathPart + 2.L * std::sqrtl(1.L - PathPart)) / std::powl(PathPart, 2.L));
+    static constexpr long double half_g = 4.9L;//ускорение свободного падения пополам
+    return half_g * ((2.L - PathPart + 2.L * std::sqrtl(1.L - PathPart)) / std::powl(PathPart, 2.L));//формула
 }
 
+//функция для ввода значения с консоли с проверкой
 template<typename T, typename... Func>
-T getValue(Func... UnPred)
+T getValue(Func... UnPred)//UnPred(Unary predicate) - функции для проверки правильности считанного значения
 {
     T  value;
     while (!(std::cin >> value) || std::cin.peek() != '\n' || (!UnPred(value) || ...))
@@ -26,25 +28,29 @@ T getValue(Func... UnPred)
     return value;
 }
 
+//Функция-обертка, вызывает getValue<double>
 template<typename... Func>
 long double getld(Func&&... UnPred)
 {
     return getValue<long double>(std::forward<Func>(UnPred)...);
 }
 
+//Функция-обертка, вызывает getValue<std::string>
 template<typename... Func>
 std::string getstr(Func&&... UnPred)
 {
     return getValue<std::string>(std::forward<Func>(UnPred)...);
 }
 
+//Узнает о намерениях пользователя о продолжении работы программы через консоль
 bool keep_on()
 {
-    static const std::array<std::string, 8> choices = { "Yes", "yes", "y", "Y", "No", "no", "n", "N" };
-    std::array<std::string, 8>::const_iterator find_result;
+    static const std::array<std::string, 8> choices = { "Yes", "yes", "y", "Y", "No", "no", "n", "N" };//допустимые ответы
+    std::array<std::string, 8>::const_iterator find_result;//указывает на введенный пользователем допустимый ответ
     auto valid = [&find_result](const std::string& str){
         return (find_result = std::find(choices.cbegin(), choices.cend(), str)) != choices.cend();
     };
+    std::cout << "Do you want to continue?\n>";
     getstr(valid);
     return  find_result < choices.cbegin() + choices.size() / 2;
 }
@@ -52,15 +58,16 @@ bool keep_on()
 int main()
 {
     std::cout << "=========Last second of the fall=========\n";
-    auto valid_val = [](long double PathPart) noexcept{
+    auto valid_val = [](long double PathPart) noexcept{//проверяет допустимость введенного пользователем значения для доли расстояния
         return PathPart > 0.L && PathPart < 1.L;
     };
     std::cout << std::setprecision(std::numeric_limits<long double>::max_digits10 + 1);
     do
     {
         std::cout << "Input a fraction of distance covered in the last second\n>";
-        long double PathPart = getld(valid_val);
-        std::cout << "Total height: " << calc_height(PathPart) << '\n';
-        std::cout << "Do you want to continue?\n>";
+        if (const auto result = calc_height(getld(valid_val)); std::isnormal(result))//если подсчёт удался
+            std::cout << "Total height: " << result << '\n';
+        else
+            std::cout << "Error! Failed to calculate the height.\n";
     } while (keep_on());
 }
