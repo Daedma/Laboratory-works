72f27030b54df37e049df940abc203a34019301d
Daedma <hdamir163@gmail.com>
2022-11-10 11:20:21 +0400

    ООП 6 ЛР

---
+++ sem3/oop/lab6/linux-build.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+javac --module-path "/home/java-libs/openjfx-19/lib" --add-modules ALL-MODULE-PATH *.java gui/*.java
---
+++ sem3/oop/lab6/linux-run.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+java --module-path "~/java-libs/openjfx-19:~/java-libs" --add-modules ALL-MODULE-PATH FunctionGUIApp
