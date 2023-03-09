bebf21b75e816340582afa2a72f7a147c08adaf7
Daedma <hdamir163@gmail.com>
2023-03-10 02:02:51 +0400

    ЛР 1 по численным методам: сделано

--- sem4/met/lab1/1_2.py
+++ sem4/met/lab1/1_2.py
@@ -1,6 +1,5 @@
 import sympy
 
-
 print("1 задание: ")
 x = sympy.Symbol('x')
 y = sympy.Symbol('y')
@@ -15,4 +14,19 @@ print(sympy.diff(expr, x))
 print(sympy.diff(expr, y))
 
 print("3 задание: ")
-sympy.integrate()
+print(sympy.integrate(expr, x))
+
+print("4 задание: ")
+print(sympy.solveset(sympy.Eq(expr.subs(x, 2), -100), x))
+
+print("5 задание:")
+system = sympy.Matrix([[-7, 3, 1, 0], [2, -1, 1, -1], [1, -2, 5, -2]])
+print(sympy.linsolve(system))
+
+print("6 задание: ")
+expr = 1/(1 + sympy.sqrt(2*x + 1));
+print(sympy.integrate(expr, (x, 0, 4)))
+
+print("7 задание: ")
+expr = sympy.cos(x + y)
+print(sympy.integrate(expr, (y, 0, x), (x, 0, sympy.pi/2)))
