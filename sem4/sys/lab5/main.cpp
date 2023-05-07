48875c21b489943c0d0b9bc47750e128a70ca040
Daedma <hdamir163@gmail.com>
2023-05-07 22:14:23 +0400

    ЛР 5 по ассемблеру: добавлены проверки

--- sem4/sys/lab5/main.cpp
+++ sem4/sys/lab5/main.cpp
@@ -14,6 +14,7 @@ int main()
 	double twei = 28;
 	double one = 1;
 
+	int error = 0;
 
 	asm volatile(
 		"finit;"              // {}
@@ -22,17 +23,47 @@ int main()
 		"fldl %[d];"          // {d, c/4}
 		"fmull %[twei];"      // {28*d, c/4}
 		"fadd;"               // {d*28 + c/4, c/4}
+
 		"fldl %[a];"          // {a, d*28 + c/4, c/4}
+
+		"fldz;"               // обработка деления на ноль
+		"fldl %[d];"
+		"fucompp;"
+		"fnstsw %%ax;"
+		"sahf;"
+		"jz error_0;"
+
 		"fdivl %[d];"         // {a/d, d*28 + c/4, c/4}
+
 		"fsubl %[c];"         // {a/d - c, d*28 + c/4, c/4}
 		"fsubl %[one];"       // {a/d - c - 1, d*28 + c/4, c/4}
+
+		"fldz;"               // обработка деления на ноль
+		"fucomp;"
+		"fnstsw %%ax;"
+		"sahf;"
+		"jz error_0;"
+
 		"fxch %%st(1);"       // {d*28 + c/4, a/d - c - 1, c/4}
 		"fdiv;"               // {(c / 4 + 28 * d) / (a / d - c - 1), d*28 + c/4, c/4}
 		"fstpl %[result];"    //
+		"jmp exit;"
+
+		"error_0:"
+		"movl $1, %[err];"
+
+		"exit:;"
 		: [result] "=m" (result)
-		: [a] "m" (a), [d] "m" (d), [c] "m" (c), [four] "m" (four), [twei] "m" (twei), [one] "m" (one)
-		: "memory"
+		: [a] "m" (a), [d] "m" (d), [c] "m" (c), [four] "m" (four), [twei] "m" (twei), [one] "m" (one), [err] "m" (error)
+		: "memory", "ax"
 		);
-	std::cout << "asm : " << result << std::endl;
+	if (error)
+	{
+		std::cout << "Division by zero\n";
+	}
+	else
+	{
+		std::cout << "asm : " << result << std::endl;
+	}
 	std::cout << "cpp : " << (c / 4 + 28 * d) / (a / d - c - 1) << std::endl;
 }
