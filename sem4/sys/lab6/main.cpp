5fe55778b39cc678f81536e0360b77cfda051f4f
Daedma <hdamir163@gmail.com>
2023-05-08 16:35:57 +0400

    ЛР 6 по ассемблеру: добавлены все проверки + комментарии

--- sem4/sys/lab6/main.cpp
+++ sem4/sys/lab6/main.cpp
@@ -29,39 +29,90 @@ int main()
 	const double c3 = 3.;
 	const double cm2 = -2.;
 
+	int error = 0;
+	short status = 0;
+
 	asm volatile(
-		"finit;"
-		"fldl %[b];"
-		"fldl %[a];"
-		"fucomi;"
+		"finit;"               // инициализация сопроцессора
+		"fldl %[b];"           // {b}
+		"fldl %[a];"           // {a, b}
+		"fucomi;"			   // сравнение a и b
 		"ja great;"
 		"jb less;"
 		// a == b
-		"fmul;"
-		"fchs;"
+		"fmul;"                // {a*b, b}
+		"fchs;"                // {-a*b, b}
 		"jmp end;"
 		// a > b
 		"great:"
-		"fadd;"
-		"fldl %[a];"
-		"fsubl %[b];"
-		"fdiv;"
+		"fadd;"                // {a + b, b}
+		"fldz;"                // {0, a + b, b}
+		"fucomip;"             // {a + b, b} # сравнение с (a + b) с нулем
+		"je error_0;"
+		"fldl %[a];"           // {a, a + b, b}
+		"fsubl %[b];"          // {a - b, a + b, b}
+		"fdiv;"                // {(a - b)/(a + b), a + b, b}
 		"jmp end;"
 		// a < b
 		"less:"
-		"fmull %[c3];"
-		"faddl %[cm2];"
-		"fdiv;"
+		"fldz;"                // {0, a, b}
+		"fucomip %%st(2);"     // {a, b} # сравнение b с нулем
+		"je error_0;"
+		"fmull %[c3];"         // {a * 3, b}
+		"faddl %[cm2];"        // {a * 3 - 2, b}
+		"fdiv;"                // {(a*3 - 2)/b, b}
 		"jmp end;"
+		// обработка ошибки деления на ноль
+		"error_0:"
+		"movl $1, %[error];"
+		"jmp exit;"
 		// запись результата
 		"end:"
 		"fstl %[result];"
+		"exit:"
+		"fxam;"
+		"fstsw %%ax;"
+		"and $0b0100010100000000, %%ax;"
+		"mov %%ax, %[status];"
 
-		: [result] "=m" (result)
+		: [result] "=m" (result), [error] "=m" (error), [status] "=m" (status)
 		: [a] "m" (a), [b] "m" (b), [c3] "m" (c3), [cm2] "m" (cm2)
-		: "memory"
+		: "memory", "ax"
 		);
 
-	std::cout << "asm : " << result << std::endl;
+	if (error)
+	{
+		std::cout << "Division by zero.\n";
+	}
+	else
+	{
+		std::cout << "asm : " << result << std::endl;
+		switch (status)
+		{
+		case 0x0:
+			std::cout << "Unsupported format\n";
+			break;
+		case 0x100:
+			std::cout << "NaN\n";
+			break;
+		case 0x400:
+			std::cout << "Finite number\n";
+			break;
+		case 0x500:
+			std::cout << "Infinity\n";
+			break;
+		case 0x4000:
+			std::cout << "Zero\n";
+			break;
+		case 0x4100:
+			std::cout << "Empty register\n";
+			break;
+		case 0x4400:
+			std::cout << "An unnormalized number\n";
+			break;
+		default:
+			break;
+		}
+	}
 	std::cout << "cpp : " << f_cpp(a, b) << std::endl;
 }
