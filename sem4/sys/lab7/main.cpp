ac02eb6c634182194c8f5af355f14102c9c35d43
Daedma <hdamir163@gmail.com>
2023-05-09 18:24:34 +0400

    ЛР 7 по ассемблеру: готовая версия

--- sem4/sys/lab7/main.cpp
+++ sem4/sys/lab7/main.cpp
@@ -1,47 +1,53 @@
 #include <iostream>
 #include <cmath>
+#include <limits>
+#include <tuple>
+#include <iomanip>
 
-double ch(double x, double eps, double value)
+std::tuple<double, size_t, double> ch(double x, double eps, double value)
 {
-	double result = 1., last = -1;
+	double result = 1.;
+	double error = -1.;
 	size_t counter = 1;
-	const double two = 2.;
-	eps *= 1.5;
+	eps *= 1.5;                   // R_n < 2/3*v_n
 	asm volatile(
 		"finit;"
 		"fldl %[eps];"            // {eps}
 		"fld1;"                   // {1, eps}
-
+		/// Начало цикла
 		"begin_loop:"             // {C_(k-1), eps}
 
-		"fldl %[x];"              // {x, C_(k-1), eps}
-		"fmul %%st;"              // {x*x, C_(k-1), eps}
-		"fildl %[counter];"       // {k, x*x, C_(k-1), eps}
-		"fmull %[two];"           // {2k, x*x, C_(k-1), eps}
-		"fld1;"                   // {1, 2k, x*x, C_(k-1), eps}
-		"fsub;"                   // {1 - 2k, 2k, x*x, C_(k-1), eps}
-		"fchs;"                   // {2k - 1, 2k, x*x, C_(k-1), eps}
-		"fmulp %%st(0), %%st(1);" // {2k(2k - 1), x*x, C_(k-1), eps}
+		"fildl %[counter];"       // {k, C_(k-1), eps}
+		"fadd %%st;"              // {2k, C_(k-1), eps}
+		"fld1;"                   // {1, 2k, C_(k-1), eps}
+		"fsub %%st(1);"           // {1 - 2k, 2k, C_(k-1), eps}
+		"fchs;"                   // {2k - 1, 2k, C_(k-1), eps}
+		"fmulp %%st(0), %%st(1);" // {2k(2k - 1), C_(k-1), eps}
+		"fldl %[x];"              // {x, 2k(2k - 1), C_(k-1), eps}
+		"fmul %%st;"              // {x*x, 2k(2k - 1), C_(k-1), eps}
 		"fdivp %%st(0), %%st(1);" // {x*x/(2k(2k - 1)), C_(k-1), eps}
 		"fmulp %%st(0), %%st(1);" // {x*x/(2k(2k - 1)) * C_(k-1), eps}
 
-		"fucomi;"
-		"jbe end_loop;"
-		"fldl %[result];"
-		"fadd;"
-		"fstpl %[result];"
-		"incq %[counter];"
+		"fucomi;"                 // сравнение с eps
+		"jbe end_loop;"           // если меньше, то заканчиваем цикл
+		"fldl %[result];"         // {result, C_k, eps}
+		"fadd %%st(1);"           // {result + C_k, C_k, eps}
+		"fstpl %[result];"        // {C_k, eps}
+		"incq %[counter];"        // инкрементируем счетчик
 
 		"jmp begin_loop;"
-		"end_loop:"
-		"fstl %[last];"
+		/// Конец цикла
+		"end_loop:"               // вычисление достигнутой погрешности
+		"fldl %[result];"         // {result, C_k, eps}
+		"fsubl %[value];"          // {result - value, C_k, eps}
+		"fabs;"                   // {|value - result|, C_k, eps}
+		"fstl %[error];"          //
 
-		: [result] "=m" (result), [counter] "+m" (counter), [last] "=m" (last)
-		: [x] "m" (x), [eps] "m" (eps), [two] "m" (two)
+		: [result] "+m" (result), [counter] "+m" (counter), [error] "=m" (error)
+		: [x] "m" (x), [eps] "m" (eps), [value] "m" (value)
 		: "memory"
 		);
-	std::cout << counter << ' ' << last << ' ';
-	return result;
+	return { result, counter, error };
 }
 
 double ch_cpp(double x, double eps)
@@ -69,9 +75,14 @@ double ch_cpp(double x, double eps)
 
 int main()
 {
-	double arg = 0.7;
-	double precision = 1.e-5;
-	std::cout << "asm : " << ch(arg, precision, 0) << std::endl;
-	std::cout << "cpp : " << ch_cpp(arg, precision) << std::endl;
-	std::cout << "std : " << std::cosh(arg) << std::endl;
+	double x, eps;
+	std::cout << "Enter x\n>";
+	std::cin >> x;
+	std::cout << "Enter precision\n>";
+	std::cin >> eps;
+	auto [result, count, error] = ch(x, eps, cosh(x));
+	constexpr auto max_precision{ std::numeric_limits<double>::digits10 + 1 };
+	std::cout << "Result                                  : " << std::setprecision(max_precision) << result << '\n';
+	std::cout << "Number of sumarized terms of the series : " << std::setprecision(max_precision) << count << '\n';
+	std::cout << "Error                                   : " << std::setprecision(max_precision) << error << '\n';
 }
