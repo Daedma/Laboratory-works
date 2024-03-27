33c094e5943da41374542082e94639665ec104ef
Daedma <hdamir163@gmail.com>
2024-03-28 03:58:59 +0400

    Реализовал многомерные варианты алгоритмов минимизации

--- sem6/om/lab2/main.cpp
+++ sem6/om/lab2/main.cpp
@@ -10,8 +10,8 @@
 #include <iostream>
 #include <functional>
 #include <cmath>
-#include <vector>
 #include <utility>
+#include "Mathter/Vector.hpp"
 
 namespace
 {
@@ -19,14 +19,33 @@ namespace
 	constexpr size_t MAX_ITERATIONS = 1703;
 }
 
+template<int N>
+using Vector = mathter::Vector<double, N>;
 
-double dihotomia(std::function<double(double)> func, double left, double right, double eps = DEFAULT_ACCURACY, size_t iterMax = MAX_ITERATIONS)
+template<int N>
+std::ostream& operator<<(std::ostream& os, const Vector<N>& rhs)
 {
-	double middle;
-	for (size_t i = 0; i != iterMax && (right - left) >= eps; ++i)
+	os << '(';
+	if (rhs.Dimension())
+	{
+		for (size_t i = 0; i != rhs.Dimension() - 1; i++)
+		{
+			os << rhs[i] << ';';
+		}
+		os << rhs[rhs.Dimension() - 1];
+	}
+	return os << ')';
+}
+
+template<int N, typename Func>
+Vector<N> dihotomia(Func func, Vector<N> left, Vector<N> right, double eps = DEFAULT_ACCURACY, size_t iterMax = MAX_ITERATIONS)
+{
+	Vector<N> middle, direction;
+	for (size_t i = 0; i != iterMax && mathter::LengthSquared(right - left) >= eps * eps; ++i)
 	{
 		middle = (right + left) * .5;
-		if (func(middle - eps) > func(middle + eps))
+		direction = mathter::Normalize(right - left) * eps;
+		if (func(middle - direction) > func(middle + direction))
 		{
 			left = middle;
 		}
@@ -35,18 +54,20 @@ double dihotomia(std::function<double(double)> func, double left, double right, 
 			right = middle;
 		}
 	}
-	return middle;
+	return (left + right) * .5;
 }
 
-double goldenRatioDihotomia(std::function<double(double)> func, double left, double right, double eps = DEFAULT_ACCURACY, size_t iterMax = MAX_ITERATIONS)
+template<int N, typename Func>
+Vector<N> goldenRatioDihotomia(Func func, Vector<N> left, Vector<N> right, double eps = DEFAULT_ACCURACY, size_t iterMax = MAX_ITERATIONS)
 {
 	constexpr double REVERSE_PHI = 0.6180339887498948;
-	double dx = right - left;
-	double x1 = right - dx * REVERSE_PHI, x2 = left + dx * REVERSE_PHI;
-	double fx1 = func(x1);
-	double fx2 = func(x2);
+	Vector<N> dx = right - left;
+	Vector<N> x1 = right - dx * REVERSE_PHI, x2 = left + dx * REVERSE_PHI;
+	size_t fx1 = func(x1);
+	size_t fx2 = func(x2);
 
-	for (size_t i = 0; i != iterMax && (x2 - x1) >= eps; ++i)
+	eps *= 4. * eps;
+	for (size_t i = 0; i != iterMax && mathter::LengthSquared(x2 - x1) >= eps; ++i)
 	{
 		if (fx1 >= fx2)
 		{
@@ -70,30 +91,37 @@ double goldenRatioDihotomia(std::function<double(double)> func, double left, dou
 	return (x2 + x1) * 0.5;
 }
 
-std::pair<size_t, size_t> getFibonacciPairAbove(double val)
+std::pair<size_t, size_t> getFibonacciPairAbove(double val, size_t& outIterations)
 {
 	size_t cur = 1, prev = 0;
 	while (cur <= val)
 	{
 		cur += prev;
 		prev = cur - prev;
+		++outIterations;
 	}
 	return { cur, prev };
 }
 
-double fibonacciMethod(std::function<double(double)> func, double left, double right, double eps = DEFAULT_ACCURACY)
+template<int N, typename Func>
+Vector<N> fibonacciMethod(Func func, Vector<N> left, Vector<N> right, double eps = DEFAULT_ACCURACY, size_t iterMax = MAX_ITERATIONS)
 {
-	auto [fn, fnm1] = getFibonacciPairAbove((right - left) / eps);
+	size_t it = 0;
+	auto [fn, fnm1] = getFibonacciPairAbove(mathter::Distance(right, left) / eps, it);
+	if (it > iterMax)
+	{
+		return (Vector<N>(NAN));
+	}
 	size_t fnm2 = fn - fnm1;
-	double dx = right - left;
-	double x1 = left + (fnm2 * dx) / fn;
-	double x2 = left + (fnm1 * dx) / fn;
+	Vector<N> dx = right - left;
+	Vector<N> x1 = left + (fnm2 * dx) / fn;
+	Vector<N>x2 = left + (fnm1 * dx) / fn;
 	double fx1 = func(x1);
 	double fx2 = func(x2);
 	fn = fnm1;
 	fnm1 = fnm2;
 	fnm2 = fn - fnm1;
-	while (fn != fnm1)
+	while (it-- != 0)
 	{
 		if (fx1 < fx2)
 		{
@@ -118,4 +146,28 @@ double fibonacciMethod(std::function<double(double)> func, double left, double r
 		fnm2 = fn - fnm1;
 	}
 	return (x1 + x2) * .5;
+}
+
+static double test_func_2(const Vector<2>& x)
+{
+	return (x[0] - 5) * x[0] + (x[1] - 3) * x[1]; // min at point x = 2.5, y = 1.5
+}
+
+int main(int argc, char const* argv[])
+{
+	std::cout << "\n////////////////////\n";
+	std::cout << "/// Lab. work #2 ///\n";
+	std::cout << "////////////////////\n\n";
+
+	Vector<2> x_0 = { 0, 0 };
+	Vector<2> x_1 = { 5, 3 };
+
+	std::cout << "{ x, y } = agrmin((x - 2) * (x - 2) + (y - 2) * (y - 2))\n";
+	std::cout << "x_0 = " << x_0 << ", x_1 = " << x_1 << "\n";
+	///  Для реализации по-координтаного спуска необходимо реализовать один из следующих трех методов для работы с vec_n
+	std::cout << "bisect                : " << dihotomia(test_func_2, x_1, x_0) << "\n";
+	std::cout << "golden_ratio          : " << goldenRatioDihotomia(test_func_2, x_1, x_0) << "\n";
+	std::cout << "fibonacci             : " << fibonacciMethod(test_func_2, x_1, x_0) << "\n";
+	std::cout << "\n";
+	return 0;
 }
