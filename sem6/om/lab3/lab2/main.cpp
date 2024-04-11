0c8d931da97a68bb54a95dfecdd39c4282487f01
Daedma <hdamir163@gmail.com>
2024-04-11 16:45:34 +0400

    OM Lab 3 work version

--- sem6/om/lab3/lab2/main.cpp
+++ sem6/om/lab3/lab2/main.cpp
@@ -41,6 +41,19 @@ inline int sgn(T val)
 }
 
 template<int N, typename Func>
+Vector<N> gradient(Func func, const Vector<N> x, double eps)
+{
+	Vector<N> result;
+	for (int i = 0; i != N; ++i)
+	{
+		Vector<N> xipe = x;
+		xipe[i] += eps;
+		result[i] = (func(xipe) - func(x)) / eps;
+	}
+	return result;
+}
+
+template<int N, typename Func>
 Vector<N> goldenRatio(Func func, Vector<N> left, Vector<N> right, double eps = DEFAULT_ACCURACY, size_t iterMax = MAX_ITERATIONS)
 {
 	constexpr double REVERSE_PHI = 0.6180339887498948;
@@ -74,6 +87,47 @@ Vector<N> goldenRatio(Func func, Vector<N> left, Vector<N> right, double eps = D
 	return (x2 + x1) * 0.5;
 }
 
+template<int N, typename Func>
+Vector<N> gradientDescent(Func func, Vector<N> start, double eps = DEFAULT_ACCURACY, size_t iterMax = MAX_ITERATIONS)
+{
+	Vector<N> cur;
+	for (size_t i = 0; i != iterMax; i++)
+	{
+		cur = goldenRatio(func,
+			start - gradient(func, start, eps), start, eps, iterMax);
+		if (mathter::LengthSquared(cur - start) < eps * eps)
+		{
+			return cur;
+		}
+		start = cur;
+	}
+	return cur;
+}
+
+template<int N, typename Func>
+Vector<N> conjGradientDescend(Func func, Vector<N> start, double eps = DEFAULT_ACCURACY, size_t iterMax = MAX_ITERATIONS)
+{
+	Vector<N> gradPrev = -gradient(func, start, eps);
+	Vector<N> gradCur;
+	Vector<N> cur;
+	double omega;
+	for (size_t i = 0; i != iterMax; ++i)
+	{
+		cur = start + gradPrev;
+		if (mathter::LengthSquared(cur - start) < eps * eps)
+		{
+			return cur;
+		}
+		cur = goldenRatio(func, cur, start, eps, iterMax);
+		gradCur = -gradient(func, cur, eps);
+		// omega = mathter::LengthSquared(gradCur) / mathter::LengthSquared(gradPrev); // Fletcher–Reeves
+		omega = std::max(0., mathter::Dot(gradCur, gradCur - gradPrev)) / mathter::LengthSquared(gradPrev); // Polak–Ribière
+		gradPrev = gradPrev * omega + gradCur;
+		start = cur;
+	}
+	return cur;
+}
+
 static double test_func_2(const Vector<2>& x)
 {
 	return (x[0] - 5) * x[0] + (x[1] - 3) * x[1]; // min at point x = 2.5, y = 1.5
@@ -82,15 +136,11 @@ static double test_func_2(const Vector<2>& x)
 int main(int argc, char const* argv[])
 {
 	std::cout << "\n////////////////////\n";
-	std::cout << "/// Lab. work #2 ///\n";
+	std::cout << "/// Lab. work #3 ///\n";
 	std::cout << "////////////////////\n\n";
-
-	Vector<2> x_0 = { 0, 0 };
-	Vector<2> x_1 = { 5, 3 };
-
-	std::cout << "{ x, y } = agrmin((x - 2) * (x - 2) + (y - 2) * (y - 2))\n";
-	std::cout << "x_0 = " << x_0 << ", x_1 = " << x_1 << "\n";
 	Vector<2> x_start = { -14, -33.98 };
 	std::cout << "x_start = " << x_start << "\n";
+	std::cout << "gradient_descend      : " << gradientDescent(test_func_2, x_start) << "\n";
+	std::cout << "conj_gradient_descend : " << conjGradientDescend(test_func_2, x_start) << "\n";
 	return 0;
 }
