6c33a399bd9950b714d707f99a1577187799cf1f
Daedma <hdamir163@gmail.com>
2024-05-30 02:21:22 +0400

    Приукрасил код + ввел отслеживание переменных

--- sem6/om/lab5/SimplexMethod.hpp
+++ sem6/om/lab5/SimplexMethod.hpp
@@ -5,6 +5,7 @@
 #include <limits>
 #include <iomanip>
 #include <numeric>
+#include <map>
 
 template<typename T>
 class SimplexMethod
@@ -35,6 +36,7 @@ private:
 	std::vector<std::vector<T>> tableau;
 	std::vector<T> solution;
 	T objectiveValue;
+	std::vector<size_t> varMapping;
 	size_t numIterations;
 	size_t m;
 	size_t n;
@@ -45,33 +47,35 @@ SimplexMethod<T>::SimplexMethod(const std::vector<std::vector<T>>& A,
 	const std::vector<T>& b,
 	const std::vector<T>& c) : m(A.size()), n(A[0].size())
 {
-	// Initialize tableau
-	tableau.resize(m + 1, std::vector<T>(n + m + 1));
+	tableau.resize(m + 1, std::vector<T>(n + m + 1, (T(0))));
 
-	// Fill tableau with A, b, and c
-	for (int i = 0; i < m; ++i)
+	for (size_t i = 0; i != m; ++i)
 	{
-		for (int j = 0; j < n; ++j)
+		for (size_t j = 0; j != n; ++j)
 		{
 			tableau[i][j] = A[i][j];
 		}
 		tableau[i][n + i] = 1;
 		tableau[i][n + m] = b[i];
 	}
-	for (int j = 0; j < n; ++j)
+	for (size_t j = 0; j != n; ++j)
 	{
 		tableau[m][j] = -c[j];
 	}
 	tableau[m][n + m] = 0;
 
-	// Initialize other variables
 	solution.resize(n);
+
+	varMapping.resize(n);
+	std::iota(varMapping.begin(), varMapping.end(), T{ 0 });
+
 	numIterations = 0;
 }
 
 template<typename T>
 bool SimplexMethod<T>::solve()
 {
+	using std::swap;
 	printTable();
 	while (!isOptimal())
 	{
@@ -86,13 +90,14 @@ bool SimplexMethod<T>::solve()
 			return false; // Unbounded solution
 		}
 		pivot(pivotRow, pivotCol);
+		varMapping[pivotCol] = pivotRow;
 		++numIterations;
 		printTable();
 	}
 
-	for (int j = 0; j < n; ++j)
+	for (size_t i = 0; i != n; ++i)
 	{
-		solution[j] = tableau[j][n + m];
+		solution[i] = tableau[varMapping[i]][n + m];
 	}
 	objectiveValue = -tableau[m][n + m];
 
@@ -104,23 +109,20 @@ void SimplexMethod<T>::pivot(int pivotRow, int pivotCol)
 {
 	T pivotElement = tableau[pivotRow][pivotCol];
 
-	// Divide the pivot row by the pivot element
-	for (int j = 0; j <= n + m; ++j)
+	for (size_t j = 0; j != n + m + 1; ++j)
 	{
 		tableau[pivotRow][j] /= pivotElement;
 	}
 
-	// Perform row operations to eliminate the pivot column
-	for (int i = 0; i < m + 1; ++i)
+	for (size_t i = 0; i != m + 1; ++i)
 	{
-		if (i == pivotRow)
+		if (i != pivotRow)
 		{
-			continue;
-		}
-		T factor = tableau[i][pivotCol];
-		for (int j = 0; j <= n + m; ++j)
-		{
-			tableau[i][j] -= factor * tableau[pivotRow][j];
+			T factor = tableau[i][pivotCol];
+			for (size_t j = 0; j != n + m + 1; ++j)
+			{
+				tableau[i][j] -= factor * tableau[pivotRow][j];
+			}
 		}
 	}
 }
@@ -131,7 +133,7 @@ int SimplexMethod<T>::findPivotRow(int pivotCol)
 	T minRatio = std::numeric_limits<T>::max();
 	int pivotRow = -1;
 
-	for (int i = 0; i < tableau.size(); ++i)
+	for (size_t i = 0; i != tableau.size(); ++i)
 	{
 		if (tableau[i][pivotCol] > 0)
 		{
@@ -153,7 +155,7 @@ int SimplexMethod<T>::findPivotCol()
 	T maxCoeff = std::numeric_limits<T>::lowest();
 	int pivotCol = -1;
 
-	for (int j = 0; j != tableau[0].size() - 1; ++j)
+	for (size_t j = 0; j != tableau[0].size() - 1; ++j)
 	{
 		if (tableau[tableau.size() - 1][j] < 0)
 		{
@@ -185,9 +187,9 @@ bool SimplexMethod<T>::isOptimal()
 template<typename T>
 void SimplexMethod<T>::printTable() const
 {
-	for (int i = 0; i < m + 1; ++i)
+	for (size_t i = 0; i != m + 1; ++i)
 	{
-		for (int j = 0; j < n + m + 1; ++j)
+		for (size_t j = 0; j != n + m + 1; ++j)
 		{
 			std::cout << std::setw(10) << tableau[i][j];
 		}
@@ -195,75 +197,3 @@ void SimplexMethod<T>::printTable() const
 	}
 	std::cout << std::endl;
 }
-
-// template<typename T>
-// void SimplexMethod<T>::printTable() const
-// {
-// 	std::cout << "Iteration " << numIterations << ":" << std::endl;
-
-// 	// Определяем максимальную ширину столбцов
-// 	int maxWidth = 0;
-// 	for (size_t j = 0; j < tableau[0].size(); ++j)
-// 	{
-// 		int width = 0;
-// 		for (size_t i = 0; i < tableau.size(); ++i)
-// 		{
-// 			width = std::max(width, static_cast<int>(std::floor(std::log10(std::abs(tableau[i][j]))) + 1));
-// 		}
-// 		maxWidth = std::max(maxWidth, width);
-// 	}
-
-// 	// Выводим заголовки таблицы
-// 	std::cout << std::left << std::setw(maxWidth + 1) << "  ";
-// 	for (size_t j = 0; j < tableau[0].size(); ++j)
-// 	{
-// 		if (j < m && tableau[0][j] == j + 1)
-// 		{
-// 			std::cout << std::left << std::setw(maxWidth + 1) << "x" << j + 1;
-// 		}
-// 		else if (j == tableau[0].size() - 1)
-// 		{
-// 			std::cout << std::left << std::setw(maxWidth + 1) << "b";
-// 		}
-// 		else
-// 		{
-// 			std::cout << std::left << std::setw(maxWidth + 1) << "";
-// 		}
-// 	}
-// 	std::cout << std::endl;
-
-// 	// Выводим строки таблицы
-// 	for (size_t i = 0; i < tableau.size(); ++i)
-// 	{
-// 		if (i < m && tableau[i][tableau[0].size() - 1] == 1)
-// 		{
-// 			std::cout << std::left << std::setw(maxWidth + 1) << "x" << i + 1 << ":";
-// 		}
-// 		else if (i == tableau.size() - 1)
-// 		{
-// 			std::cout << std::left << std::setw(maxWidth + 1) << "f:";
-// 		}
-// 		else
-// 		{
-// 			std::cout << std::left << std::setw(maxWidth + 1) << "";
-// 		}
-// 		for (size_t j = 0; j < tableau[i].size(); ++j)
-// 		{
-// 			std::cout << std::right << std::setw(maxWidth + 1) << tableau[i][j];
-// 		}
-// 		std::cout << std::endl;
-// 	}
-
-// 	// Выводим значения базисных переменных
-// 	std::cout << "Basic variables:" << std::endl;
-// 	for (size_t i = 0; i < m; ++i)
-// 	{
-// 		if (tableau[i][tableau[0].size() - 1] == 1)
-// 		{
-// 			std::cout << std::left << std::setw(maxWidth + 1) << "x" << i + 1 << ": " << solution[i] << std::endl;
-// 		}
-// 	}
-// 	std::cout << std::left << std::setw(maxWidth + 1) << "f: " << -objectiveValues.back() << std::endl;
-// }
-
-
--- sem6/om/lab5/main.cpp
+++ sem6/om/lab5/main.cpp
@@ -5,17 +5,13 @@
 
 int main()
 {
-	// std::cout << " f(x,c) = -2x1 + 3x2;\n arg_min = {7, 0}, f(arg_min) =-14\n\n";
-// Создаем матрицу ограничений A и векторы C и B
 	std::vector<std::vector<double>> A = { { -2, 6 }, { 3, 2 }, { 2, -1 } };
 	std::vector<double> C = { 2, 3 };
 	std::vector<double> B = { 40, 28, 14 };
 
-	// Создаем экземпляр класса SimplexMethod и вызываем метод solve
 	SimplexMethod<double> simplex(A, B, C);
 	if (simplex.solve())
 	{
-// Выводим результаты, если решение найдено
 		std::cout << "Optimal solution found:" << std::endl;
 		std::cout << "Objective value: " << -simplex.getObjectiveValue() << std::endl;
 		std::cout << "Variables:" << std::endl;
@@ -26,7 +22,6 @@ int main()
 	}
 	else
 	{
-	 // Выводим сообщение об ошибке, если решение не найдено
 		std::cout << "No optimal solution found." << std::endl;
 	}
 
