9373c3b367a8f3536f0989fa0aa3806b65a7f6f3
Daedma <hdamir163@gmail.com>
2024-10-07 21:14:12 +0400

    лр 1 по матмоделированию: реализован функционал для проведения моделирования.

--- sem7/mm/lab1/PlanetSystem.cpp
+++ sem7/mm/lab1/PlanetSystem.cpp
@@ -7,7 +7,7 @@ namespace
 
 void PlanetSystem::makeEulerStep(size_t step)
 {
-	size_t planetsCount = coordinates.size();
+	size_t planetsCount = planets.size();
 	for (size_t i = 0; i != planetsCount; ++i)
 	{
 		Point an = calcAcceleration(i, step);
@@ -29,7 +29,7 @@ void PlanetSystem::makeEulerCromerStep(size_t step)
 
 void PlanetSystem::makeVerletStep(size_t step)
 {
-	size_t planetsCount = coordinates.size();
+	size_t planetsCount = planets.size();
 	for (size_t i = 0; i != planetsCount; ++i)
 	{
 		Point an = calcAcceleration(i, step);
@@ -47,7 +47,7 @@ void PlanetSystem::makeBeemanStep(size_t step)
 	makeEulerStep(step);
 	if (step != 0)
 	{
-		size_t planetsCount = coordinates.size();
+		size_t planetsCount = planets.size();
 		for (size_t i = 0; i != planetsCount; ++i)
 		{
 			Point an = calcAcceleration(i, step);
@@ -59,4 +59,125 @@ void PlanetSystem::makeBeemanStep(size_t step)
 				(2 * anp1 + 5 * an - anm1) / 6 * timeStep;
 		}
 	}
+}
+
+double PlanetSystem::getEnergyValue() const
+{
+	size_t lastStep = getLastStep();
+	double totalEnergy = 0.0;
+	size_t planetsCount = planets.size();
+
+	// вычисление кинетической энергии
+	for (size_t i = 0; i != planetsCount; ++i)
+	{
+		double kineticEnergy = 0.5 * planets[i].mass * boost::qvm::mag_sqr(velocities[i][lastStep]);
+		totalEnergy += kineticEnergy;
+	}
+	// вычисление потенциальной энергии
+	for (size_t i = 0; i != planetsCount; ++i)
+	{
+		Point icoords = coordinates[i][lastStep];
+		double imass = planets[i].mass;
+		for (size_t j = i + 1; j != planetsCount; ++j)
+		{
+			double r = boost::qvm::mag(icoords - coordinates[j][lastStep]);
+			if (r > 0)
+			{
+				double potentialEnergy = -G * imass * planets[j].mass / r;
+				totalEnergy += potentialEnergy;
+			}
+		}
+	}
+
+	return totalEnergy;
+}
+
+PlanetSystem::Point PlanetSystem::getVelocityValue() const
+{
+	size_t lastStep = getLastStep();
+	Point totalVelocity{ 0., 0. };
+	double totalMass = 0.0;
+	size_t planetsCount = planets.size();
+	for (size_t i = 0; i != planetsCount; ++i)
+	{
+		totalVelocity += planets[i].mass * velocities[i][lastStep];
+		totalMass += planets[i].mass;
+	}
+	if (totalMass > 0)
+	{
+		totalVelocity /= totalMass;
+	}
+	return totalVelocity;
+}
+
+PlanetSystem::Point PlanetSystem::calcAcceleration(size_t nplanet, size_t step) const
+{
+	Point acceleration{ 0., 0. };
+	size_t planetsCount = planets.size();
+	for (size_t i = 0; i != planetsCount; ++i)
+	{
+		if (i != nplanet)
+		{
+			Point distanceVector = planets[i].pos - planets[nplanet].pos;
+			double sqrDistance = boost::qvm::mag_sqr(distanceVector);
+			if (sqrDistance > 0)
+			{ // Avoid division by zero
+				Point direction = distanceVector / std::sqrt(sqrDistance);
+				acceleration += G * planets[i].mass / sqrDistance * direction;
+			}
+		}
+	}
+	return acceleration;
+}
+
+void PlanetSystem::allocMemory()
+{
+	size_t stepsCount = std::ceil(simulationTime / timeStep);
+	coordinates.resize(planets.size());
+	for (auto& i : coordinates)
+	{
+		i.resize(stepsCount);
+	}
+	velocities.resize(planets.size());
+	for (auto& i : velocities)
+	{
+		i.resize(stepsCount);
+	}
+}
+
+void PlanetSystem::setInitialConditions()
+{
+	for (size_t i = 0; i != planets.size(); ++i)
+	{
+		coordinates[i][0] = planets[i].pos;
+		velocities[i][0] = planets[i].velocity;
+	}
+}
+
+void PlanetSystem::run()
+{
+	isValidSetup();
+	allocMemory();
+	setInitialConditions();
+	workThread.reset(new std::thread{
+		[this]() {
+			size_t stepsCount = std::ceil(simulationTime / timeStep);
+			size_t curStep = 0;
+			this->inProgress = true;
+			while (curStep != stepsCount)
+			{
+				(this->*stepper)(curStep);
+				++curStep;
+				this->previousStep = curStep - 1;
+			}
+			this->inProgress = false;
+		}
+		});
+}
+
+void PlanetSystem::stop()
+{
+	workThread->detach();
+	workThread.reset();
+	inProgress = false;
 }
--- sem7/mm/lab1/PlanetSystem.hpp
+++ sem7/mm/lab1/PlanetSystem.hpp
@@ -3,11 +3,14 @@
 #include <atomic>
 #include <thread>
 #include <memory>
+#include <stdexcept>
 #include "qvm_lite.hpp"
 
 class PlanetSystem
 {
 public:
+	using Point = boost::qvm::vec<double, 2>;
+
 	enum DiffSchemes
 	{
 		EULER, VERLET, EULER_CROMER, BEEMAN
@@ -15,17 +18,15 @@ public:
 
 	struct PlanetParams
 	{
-		double posX;
-		double posY;
-		double velocityX;
-		double velocityY;
+		Point pos;
+		Point velocity;
 		double mass;
 	};
 
-	using Point = boost::qvm::vec<double, 2>;
 
 	void setDiffScheme(DiffSchemes type)
 	{
+		throwExceptionIfInProgress();
 		switch (type)
 		{
 		case DiffSchemes::EULER:
@@ -47,11 +48,13 @@ public:
 
 	void setTimeStep(double step)
 	{
+		throwExceptionIfInProgress();
 		timeStep = step;
 	}
 
 	void setSimulationTime(double time)
 	{
+		throwExceptionIfInProgress();
 		simulationTime = time;
 	}
 
@@ -80,14 +83,14 @@ public:
 
 private:
 	// Concurency
-	std::atomic_bool inProgress;
-	std::atomic_size_t previousStep;
+	std::atomic_bool inProgress = false;
+	std::atomic_size_t previousStep = -1;
 	std::unique_ptr<std::thread> workThread;
 
 	// Initial parameters
-	double timeStep;
-	void(PlanetSystem::* stepper)(size_t);
-	double simulationTime;
+	double timeStep = 0.;
+	void(PlanetSystem::* stepper)(size_t) = nullptr;
+	double simulationTime = 0.;
 	std::vector<PlanetParams> planets;
 
 	// Result
@@ -104,4 +107,54 @@ private:
 	void makeEulerCromerStep(size_t step);
 
 	void makeBeemanStep(size_t step);
+
+	void allocMemory();
+
+	void setInitialConditions();
+
+	void isValidSetup() const
+	{
+		if (stepper == nullptr)
+		{
+			throw std::runtime_error("Differential scheme is not set.");
+		}
+		if (timeStep <= 0)
+		{
+			throw std::runtime_error("Time step must be positive.");
+		}
+		if (simulationTime <= 0)
+		{
+			throw std::runtime_error("Simulation time must be positive.");
+		}
+		if (planets.empty())
+		{
+			throw std::runtime_error("No planets in the system.");
+		}
+
+		// Check if all planets have valid parameters
+		for (const auto& planet : planets)
+		{
+			using namespace boost::qvm;
+			// Check if the mass is positive
+			if (planet.mass <= 0)
+			{
+				throw std::runtime_error("Planet mass must be positive.");
+			}
+
+			// Check if the position and velocity are valid (not NaN or infinite)
+			if (!std::isfinite(X(planet.pos)) || !std::isfinite(Y(planet.pos)) ||
+				!std::isfinite(X(planet.velocity)) || !std::isfinite(Y(planet.velocity)))
+			{
+				throw std::runtime_error("Planet mass must be finite.");
+			}
+		}
+	}
+
+	void throwExceptionIfInProgress() const
+	{
+		if (inProgress)
+		{
+			throw std::runtime_error{ "Attempt to change attribute values ​​during modeling process!" };
+		}
+	}
 };
