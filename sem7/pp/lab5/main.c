5d7de902b7f20088eb1daa51b5df151d9f8dbead
Daedma <hdamir163@gmail.com>
2024-10-30 20:02:38 +0400

    ЛР 5 по ПП : сделано

--- sem7/pp/lab5/main.c
+++ sem7/pp/lab5/main.c
@@ -5,157 +5,260 @@
 #include <stdlib.h>
 #include <time.h>
 
+#ifndef N
+#define N 300
+#endif
+
+#if defined(TYPE_DOUBLE)
+#define TYPE double
+#define GEMMFUNC cublasDgemm
+#define GENERATE_UNIFORM_FUNC curandGenerateUniformDouble
+#elif defined(TYPE_FLOAT)
+#define TYPE float
+#define GEMMFUNC cublasSgemm
+#define GENERATE_UNIFORM_FUNC curandGenerateUniform
+#elif defined(TYPE_COMPLEX)
+#define TYPE cuComplex
+#define SUBTYPE float
+#define GEMMFUNC cublasCgemm
+#define GENERATE_UNIFORM_FUNC curandGenerateUniform
+#define NUM_OF_DIM 2
+#define ALPHA make_cuComplex(1, 0)
+#define BETA make_cuComplex(0, 0)
+#else
+#define TYPE double
+#define GEMMFUNC cublasDgemm
+#define GENERATE_UNIFORM_FUNC curandGenerateUniformDouble
+#endif
+
+#ifndef TYPE_COMPLEX
+#define SUBTYPE TYPE
+#define NUM_OF_DIM 1
+#define ALPHA (TYPE)1
+#define BETA (TYPE)0
+#endif
+
+#define TRANSPOSE_A
+
+#define TRANSPOSE_B
+
+#ifdef TRANSPOSE_A
+#define A_OP CUBLAS_OP_T
+#define TRANSPOSE_A_INFO "T"
+#else
+#define A_OP CUBLAS_OP_N
+#define TRANSPOSE_A_INFO "non-T"
+#endif
+
+#ifdef TRANSPOSE_B
+#define B_OP CUBLAS_OP_T
+#define TRANSPOSE_B_INFO "T"
+#else
+#define B_OP CUBLAS_OP_N
+#define TRANSPOSE_B_INFO "non-T"
+#endif
+
+#define TRANSPOSE_INFO "(" TRANSPOSE_A_INFO ", " TRANSPOSE_B_INFO ")"
+
+#define STR(x) STR_HELPER(x)
+#define STR_HELPER(x) #x
+
+#define PRINTABLE_ELEMENT_COUNT 6
+
+// Функция проверки ошибок CUDA
+#define CHECK_CUDA(call) do { \
+    cudaError_t err = call; \
+    if (err != cudaSuccess) { \
+        fprintf(stderr, "CUDA error %s:%d: %s\n", __FILE__, __LINE__, cudaGetErrorString(err)); \
+        exit(1); \
+    } \
+} while (0)
+
 // Функция случайной генерации матрицы на хосте
-void CPU_fill_rand(float *A, int nr_rows_A, int nr_cols_A) {
-  for (int i = 0; i < nr_rows_A * nr_cols_A; ++i) {
-    A[i] = rand() / (float)RAND_MAX;
-  }
+void CPU_fill_rand(TYPE* A, int nr_rows_A, int nr_cols_A)
+{
+	for (int i = 0; i < nr_rows_A * nr_cols_A * NUM_OF_DIM; ++i)
+	{
+		((SUBTYPE*)A)[i] = rand() / (SUBTYPE)RAND_MAX;
+	}
 }
 
 // Функция случайной генерации матрицы на GPU
-void GPU_fill_rand(float *A, int nr_rows_A, int nr_cols_A) {
-  curandGenerator_t prng;
-  curandCreateGenerator(&prng, CURAND_RNG_PSEUDO_DEFAULT);
-  curandSetPseudoRandomGeneratorSeed(prng, (unsigned long long)clock());
-  curandGenerateUniform(prng, A, nr_rows_A * nr_cols_A);
-  curandDestroyGenerator(prng);
+void GPU_fill_rand(TYPE* A, int nr_rows_A, int nr_cols_A)
+{
+	curandGenerator_t prng;
+	curandCreateGenerator(&prng, CURAND_RNG_PSEUDO_DEFAULT);
+	curandSetPseudoRandomGeneratorSeed(prng, (unsigned long long)clock());
+	GENERATE_UNIFORM_FUNC(prng, (SUBTYPE*)A, nr_rows_A * nr_cols_A * NUM_OF_DIM);
+	curandDestroyGenerator(prng);
 }
 
 // Функция умножения матриц на GPU
-void gpu_blas_mmul(const float *A, const float *B, float *C, const int m,
-                   const int k, const int n) {
-  int lda = m, ldb = k, ldc = m;
-  const float alf = 1;
-  const float bet = 0;
-  const float *alpha = &alf;
-  const float *beta = &bet;
-  cublasHandle_t handle;
-  cublasCreate(&handle);
-  cublasSgemm(handle, CUBLAS_OP_N, CUBLAS_OP_N, m, n, k, alpha, A, lda, B, ldb,
-              beta, C, ldc);
-  cublasDestroy(handle);
+void gpu_blas_mmul(const TYPE* A, const TYPE* B, TYPE* C, const int m,
+	const int k, const int n)
+{
+	static const TYPE alpha = ALPHA;
+	static const TYPE beta = BETA;
+	int lda = m, ldb = k, ldc = m;
+	cublasHandle_t handle;
+	cublasCreate(&handle);
+	GEMMFUNC(handle, A_OP, B_OP, m, n, k, &alpha, A, lda, B, ldb,
+		&beta, C, ldc);
+	cublasDestroy(handle);
 }
 
 // Функция вывода матрицы
-void print_matrix(float *A, int nr_rows_A, int nr_cols_A) {
-  for (int i = 0; i < nr_rows_A; ++i) {
-    for (int j = 0; j < nr_cols_A; ++j) {
-      printf("%f ", A[i * nr_cols_A + j]);
-    }
-    printf("\n");
-  }
+void print_matrix(TYPE* A, int nr_rows_A, int nr_cols_A)
+{
+	int prowc = nr_rows_A < PRINTABLE_ELEMENT_COUNT ? nr_rows_A : PRINTABLE_ELEMENT_COUNT;
+	int pcolc = nr_cols_A < PRINTABLE_ELEMENT_COUNT ? nr_cols_A : PRINTABLE_ELEMENT_COUNT;
+	for (int i = 0; i < prowc; ++i)
+	{
+		for (int j = 0; j < pcolc; ++j)
+		{
+#ifndef TYPE_COMPLEX
+			printf("%.2f ", A[i * nr_cols_A + j]);
+#else
+			printf("(%.2f; %.2f) ", A[i * nr_cols_A + j].x, A[i * nr_cols_A + j].y);
+#endif
+		}
+		printf("\n");
+	}
 }
 
-int main() {
-  srand(time(0));
+int main()
+{
+	// Вывод параметров индивидуального варианта
+	printf("Type : %s\n", STR(TYPE));
+	printf("N : %d\n", N);
+	printf(TRANSPOSE_INFO "\n");
 
-  int nr_rows_A, nr_cols_A, nr_rows_B, nr_cols_B, nr_rows_C, nr_cols_C;
-  nr_rows_A = nr_cols_A = nr_rows_B = nr_cols_B = nr_rows_C = nr_cols_C = 3;
+	srand(time(0));
 
-  float *h_A = (float *)malloc(nr_rows_A * nr_cols_A * sizeof(float));
-  float *h_B = (float *)malloc(nr_rows_B * nr_cols_B * sizeof(float));
-  float *h_C = (float *)malloc(nr_rows_C * nr_cols_C * sizeof(float));
+	int nr_rows_A, nr_cols_A, nr_rows_B, nr_cols_B, nr_rows_C, nr_cols_C;
+	nr_rows_A = nr_cols_A = nr_rows_B = nr_cols_B = nr_rows_C = nr_cols_C = N;
 
-  float *d_A, *d_B, *d_C;
-  cudaMalloc(&d_A, nr_rows_A * nr_cols_A * sizeof(float));
-  cudaMalloc(&d_B, nr_rows_B * nr_cols_B * sizeof(float));
-  cudaMalloc(&d_C, nr_rows_C * nr_cols_C * sizeof(float));
+	TYPE* h_A = (TYPE*)malloc(nr_rows_A * nr_cols_A * sizeof(TYPE));
+	TYPE* h_B = (TYPE*)malloc(nr_rows_B * nr_cols_B * sizeof(TYPE));
+	TYPE* h_C = (TYPE*)malloc(nr_rows_C * nr_cols_C * sizeof(TYPE));
 
-  float tMh = 0, tMd = 0, ts = 0, ttr = 0, tcu = 0;
-  cudaEvent_t start, stop;
-  cudaEventCreate(&start);
-  cudaEventCreate(&stop);
+	TYPE* d_A, * d_B, * d_C;
+	CHECK_CUDA(cudaMalloc(&d_A, nr_rows_A * nr_cols_A * sizeof(TYPE)));
+	CHECK_CUDA(cudaMalloc(&d_B, nr_rows_B * nr_cols_B * sizeof(TYPE)));
+	CHECK_CUDA(cudaMalloc(&d_C, nr_rows_C * nr_cols_C * sizeof(TYPE)));
 
-  for (int i = 0; i < 20; ++i) {
-    // Создание матриц на хосте
-    cudaEventRecord(start, 0);
-    CPU_fill_rand(h_A, nr_rows_A, nr_cols_A);
-    CPU_fill_rand(h_B, nr_rows_B, nr_cols_B);
-    cudaEventRecord(stop, 0);
-    cudaEventSynchronize(stop);
-    float tMh_temp;
-    cudaEventElapsedTime(&tMh_temp, start, stop);
-    tMh += tMh_temp;
+	double tMh = 0, tMd = 0, ts = 0, ttr = 0, tcu = 0;
+	clock_t start_time, end_time;
+	cudaEvent_t start, stop;
+	float gpuTime = 0.0f;
+	CHECK_CUDA(cudaEventCreate(&start));
+	CHECK_CUDA(cudaEventCreate(&stop));
 
-    // Создание матриц на девайсе
-    cudaEventRecord(start, 0);
-    GPU_fill_rand(d_A, nr_rows_A, nr_cols_A);
-    GPU_fill_rand(d_B, nr_rows_B, nr_cols_B);
-    cudaEventRecord(stop, 0);
-    cudaEventSynchronize(stop);
-    float tMd_temp;
-    cudaEventElapsedTime(&tMd_temp, start, stop);
-    tMd += tMd_temp;
+	for (int i = 0; i < 20; ++i)
+	{
+		// Создание матриц на хосте
+		start_time = clock();
+		CPU_fill_rand(h_A, nr_rows_A, nr_cols_A);
+		CPU_fill_rand(h_B, nr_rows_B, nr_cols_B);
+		end_time = clock();
+		tMh += (double)(end_time - start_time) / CLOCKS_PER_SEC;
 
-    // Последовательный алгоритм
-    cudaEventRecord(start, 0);
-    for (int i = 0; i < nr_rows_A; ++i) {
-      for (int j = 0; j < nr_cols_B; ++j) {
-        h_C[i * nr_cols_B + j] = 0;
-        for (int k = 0; k < nr_cols_A; ++k) {
-          h_C[i * nr_cols_B + j] +=
-              h_A[i * nr_cols_A + k] * h_B[k * nr_cols_B + j];
-        }
-      }
-    }
-    cudaEventRecord(stop, 0);
-    cudaEventSynchronize(stop);
-    float ts_temp;
-    cudaEventElapsedTime(&ts_temp, start, stop);
-    ts += ts_temp;
+		// Создание матриц на девайсе
+		CHECK_CUDA(cudaEventRecord(start, 0));
+		GPU_fill_rand(d_A, nr_rows_A, nr_cols_A);
+		GPU_fill_rand(d_B, nr_rows_B, nr_cols_B);
+		CHECK_CUDA(cudaDeviceSynchronize());
+		CHECK_CUDA(cudaEventRecord(stop, 0));
+		CHECK_CUDA(cudaEventElapsedTime(&gpuTime, start, stop));
+		tMd += gpuTime / 1000.;
 
-    // Передача данных на видеокарту
-    cudaEventRecord(start, 0);
-    cudaMemcpy(d_A, h_A, nr_rows_A * nr_cols_A * sizeof(float),
-               cudaMemcpyHostToDevice);
-    cudaMemcpy(d_B, h_B, nr_rows_B * nr_cols_B * sizeof(float),
-               cudaMemcpyHostToDevice);
-    cudaEventRecord(stop, 0);
-    cudaEventSynchronize(stop);
-    float ttr_temp;
-    cudaEventElapsedTime(&ttr_temp, start, stop);
-    ttr += ttr_temp;
+		// Последовательный алгоритм
+		start_time = clock();
+		for (int i = 0; i < nr_rows_A; ++i)
+		{
+			for (int j = 0; j < nr_cols_B; ++j)
+			{
+#ifndef TYPE_COMPLEX
+				h_C[i * nr_cols_B + j] = 0;
+#else
+				h_C[i * nr_cols_B + j].x = 0;
+				h_C[i * nr_cols_B + j].y = 0;
+#endif
+				for (int k = 0; k < nr_cols_A; ++k)
+				{
+#ifndef TYPE_COMPLEX
+					h_C[i * nr_cols_B + j] +=
+						h_A[i * nr_cols_A + k] * h_B[k * nr_cols_B + j];
+#else
+					h_C[i * nr_cols_B + j] = cuCfmaf(h_A[i * nr_cols_A + k], h_B[k * nr_cols_B + j], h_C[i * nr_cols_B + j]);
+#endif
+				}
+			}
+		}
+		end_time = clock();
+		ts += (double)(end_time - start_time) / CLOCKS_PER_SEC;
 
-    // Умножение матриц на GPU
-    cudaEventRecord(start, 0);
-    gpu_blas_mmul(d_A, d_B, d_C, nr_rows_A, nr_cols_A, nr_cols_B);
-    cudaEventRecord(stop, 0);
-    cudaEventSynchronize(stop);
-    float tcu_temp;
-    cudaEventElapsedTime(&tcu_temp, start, stop);
-    tcu += tcu_temp;
-  }
+		printf("Sequantional multiplication:\n");
+		print_matrix(h_C, N, N);
 
-  tMh /= 20;
-  tMd /= 20;
-  ts /= 20;
-  ttr /= 20;
-  tcu /= 20;
+		// Передача данных на видеокарту
+		CHECK_CUDA(cudaEventRecord(start, 0));
+		CHECK_CUDA(cudaMemcpy(d_A, h_A, nr_rows_A * nr_cols_A * sizeof(TYPE),
+			cudaMemcpyHostToDevice));
+		CHECK_CUDA(cudaMemcpy(d_B, h_B, nr_rows_B * nr_cols_B * sizeof(TYPE),
+			cudaMemcpyHostToDevice));
+		CHECK_CUDA(cudaDeviceSynchronize());
+		CHECK_CUDA(cudaEventRecord(stop, 0));
+		CHECK_CUDA(cudaEventElapsedTime(&gpuTime, start, stop));
+		ttr += gpuTime / 1000.;
 
-  float acu = ts / tcu;
-  float aMhcu = (tMh + ttr + tcu) / ts;
-  float aMdcu = (tMd + tcu) / ts;
+		// Умножение матриц на GPU
+		CHECK_CUDA(cudaEventRecord(start, 0));
+		gpu_blas_mmul(d_A, d_B, d_C, nr_rows_A, nr_cols_A, nr_cols_B);
+		CHECK_CUDA(cudaDeviceSynchronize());
+		CHECK_CUDA(cudaEventRecord(stop, 0));
+		CHECK_CUDA(cudaEventElapsedTime(&gpuTime, start, stop));
+		tcu += gpuTime / 1000.;
 
-  printf("Average times:\n");
-  printf("tMh: %f ms\n", tMh);
-  printf("tMd: %f ms\n", tMd);
-  printf("ts: %f ms\n", ts);
-  printf("ttr: %f ms\n", ttr);
-  printf("tcu: %f ms\n", tcu);
-  printf("Accelerations:\n");
-  printf("acu: %f\n", acu);
-  printf("aMhcu: %f\n", aMhcu);
-  printf("aMdcu: %f\n", aMdcu);
+		CHECK_CUDA(cudaMemcpy(h_C, d_C, nr_rows_C * nr_cols_C * sizeof(TYPE),
+			cudaMemcpyDeviceToHost));
 
-  // Free GPU memory
-  cudaFree(d_A);
-  cudaFree(d_B);
-  cudaFree(d_C);
+		printf("CuBLAS multiplication:\n");
+		print_matrix(h_C, N, N);
+	}
 
-  // Free CPU memory
-  free(h_A);
-  free(h_B);
-  free(h_C);
+	tMh /= 20;
+	tMd /= 20;
+	ts /= 20;
+	ttr /= 20;
+	tcu /= 20;
 
-  return 0;
+	float acu = ts / tcu;
+	float aMhcu = (tMh + ttr + tcu) / ts;
+	float aMdcu = (tMd + tcu) / ts;
+
+	printf("Matrix creation on host time : %f\n", tMh);
+	printf("Matrix creation on device time: %f\n", tMd);
+	printf("Sequantional time: %f\n", ts);
+	printf("Transfer time: %f\n", ttr);
+	printf("CuBLAS time: %f\n", tcu);
+	printf("Speedup (without transfer): %f\n", acu);
+	printf("Speedup (with creation on host and transfer): %f\n", aMhcu);
+	printf("Speedup (with creation on device): %f\n", aMdcu);
+
+	// Free GPU memory
+	CHECK_CUDA(cudaFree(d_A));
+	CHECK_CUDA(cudaFree(d_B));
+	CHECK_CUDA(cudaFree(d_C));
+
+	// Destroy CUDA events
+	CHECK_CUDA(cudaEventDestroy(start));
+	CHECK_CUDA(cudaEventDestroy(stop));
+
+	// Free CPU memory
+	free(h_A);
+	free(h_B);
+	free(h_C);
+
+	return 0;
 }
