a847cd086801302fb2c167c2891ad04ad901fb85
Daedma <hdamir163@gmail.com>
2025-04-06 22:31:12 +0400

    ЛР 2 по ОИ : сделал первое задание.

--- sem8/oisat/lab2/focus.cpp
+++ sem8/oisat/lab2/focus.cpp
@@ -19,6 +19,8 @@ namespace
 	constexpr size_t RAYS_COUNT = 9;
 
 	constexpr double PRECISION = 1.e-6;
+
+	constexpr double RAYS_DISTANCE = 1.;
 }
 
 std::pair<size_t, size_t> fibonacci_pair_above(double val)
@@ -110,7 +112,7 @@ double sd(const std::vector<vec_t>& points)
 // Find focus of a lens using sco of intersection points minimization
 double focus_ps(const biconvex_lens& lens_, double refr_ind_, double max_z_)
 {
-	auto ray_traces = trace_rays_through_lens(lens_, 1., refr_ind_, RAYS_COUNT, 300, 2);
+	auto ray_traces = trace_rays_through_lens(lens_, 1., refr_ind_, RAYS_COUNT, RAYS_DISTANCE, 2);
 
 	auto intersection_points_sd = [&ray_traces](double z_) {
 		std::vector<vec_t> intersections;
@@ -137,7 +139,7 @@ double focus_ps(const biconvex_lens& lens_, double refr_ind_, double max_z_)
 // Find focus of a lens using sco of intersection points with optical path length minimization
 double focus_popls(const biconvex_lens& lens_, double refr_ind_, double max_z_)
 {
-	auto ray_traces = trace_rays_through_lens(lens_, 1., refr_ind_, RAYS_COUNT, 300, 2);
+	auto ray_traces = trace_rays_through_lens(lens_, 1., refr_ind_, RAYS_COUNT, RAYS_DISTANCE, 2);
 
 	auto popl_sd = [&ray_traces, refr_ind_](double z_) {
 		std::vector<vec_t> intersections;
@@ -179,7 +181,7 @@ double focus_popls(const biconvex_lens& lens_, double refr_ind_, double max_z_)
 // Find focus of a lens using sco of consistent optical path length minimization
 double focus_copls(const biconvex_lens& lens_, double refr_ind_, double max_z_)
 {
-	auto ray_traces = trace_rays_through_lens(lens_, 1., refr_ind_, RAYS_COUNT, 300, 2);
+	auto ray_traces = trace_rays_through_lens(lens_, 1., refr_ind_, RAYS_COUNT, RAYS_DISTANCE, 2);
 
 	auto copl_points = [&ray_traces, refr_ind_](double h_) {
 		std::vector<vec_t> points;
@@ -217,20 +219,38 @@ int main()
 	LibBoard::Board board;
 	board.setFillColor(LibBoard::Color::White);
 	board.setPenColor(LibBoard::Color::White);
-	board.setLineWidth(2.);
-	board.fillRectangle(-1000, -1000, 2000, 2000);
+	board.setLineWidth(0.02);
 	board.moveCenter({ 0., 0. });
 
-	double lens_offset = 50;
-	biconvex_lens lens(80, 300, lens_offset, 100, 300, lens_offset);
+	double a1, a2;
+	double b1, b2;
+	double center_1, center_2;
+	double refr_ind;
 
-	double fps = focus_ps(lens, 1.5, 1000);
-	double fpopls = focus_popls(lens, 1.5, 1000);
-	double fcopls = focus_copls(lens, 1.5, 1000);
+	std::cout << "a - radius of ellipse x and y; b - radius of ellipse z;" << std::endl;
+	std::cout << "The centers of elliptical surfaces are positive and denote a shift from the origin." << std::endl;
+	std::cout << "(a1, b1) > ";
+	std::cin >> a1 >> b1;
+	std::cout << "(a2, b2) > ";
+	std::cin >> a2 >> b2;
+	std::cout << "centers of elliptical surfaces (1, 2) > ";
+	std::cin >> center_1 >> center_2;
+	std::cout << "refractive index of lens> ";
+	std::cin >> refr_ind;
+
+	biconvex_lens lens(a1, b1, center_1, a2, b2, center_2);
+
+	double fps = focus_ps(lens, refr_ind, 10);
+	double fpopls = focus_popls(lens, refr_ind, 10);
+	double fcopls = focus_copls(lens, refr_ind, 10);
+
+	std::cout << "Focus (sd of points) : " << fps << " (red dot)" << std::endl;
+	std::cout << "Focus (sd of points and optical path length) : " << fpopls << " (green dot)" << std::endl;
+	std::cout << "Focus (sd of consistent optical path length) : " << fcopls << " (blue dot)" << std::endl;
 
 	lens.draw(board, LibBoard::Color::DarkCyan);
 
-	auto ray_traces = trace_rays_through_lens(lens, 1., 1.5, 9, 500, 2);
+	auto ray_traces = trace_rays_through_lens(lens, 1., 1.5, 9, RAYS_DISTANCE, 2);
 
 	std::array<LibBoard::Color, 2> colors = {
 		LibBoard::Color::Red,
@@ -245,10 +265,10 @@ int main()
 			vec_t end = ray_trace[i + 1].origin();
 			ray_trace[i].draw(board, color, end);
 		}
-		ray_trace.back().draw(board, LibBoard::Color::Blue, 500);
+		ray_trace.back().draw(board, LibBoard::Color::Blue, 3);
 	}
 
-	board.setLineWidth(10.);
+	board.setLineWidth(0.1);
 
 	board.setPenColor(LibBoard::Color::Red);
 	board.drawDot(fps, 0);
@@ -259,8 +279,8 @@ int main()
 	board.setPenColor(LibBoard::Color::Blue);
 	board.drawDot(fcopls, 0);
 
-	board.saveSVG("lens.svg");
-	std::cout << "Lens illustration saved as lens.svg" << std::endl;
+	board.saveSVG("focus.svg");
+	std::cout << "Lens illustration saved as focus.svg" << std::endl;
 
 	return 0;
 }
